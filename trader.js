b61fcb6b4811d06e500fe1e6c0af9c05b7508397
nathannoob <nathannoob@gmail.com>
2018-02-11 03:31:29 -0500

    remade without official api. sending REST calls directly

--- trader.js
+++ trader.js
@@ -1,126 +1,95 @@
-
-const gdax = require('gdax');
-const apiRest = 'https://api.gdax.com';
-const apiFeed = 'wss://ws-feed.gdax.com';
-const sandboxRest = 'https://api-public.sandbox.gdax.com';
-const sandboxFeed = 'wss://ws-feed-public.sandbox.gdax.com';
-const ethToUsd = 'ETH-USD';
-const btcToUsd = 'BTC-USD';
-const passphrase = 'phrase';
-// const apiKey = 'apiKey';
-const base64secret = 'secret';
-const publicClient = new gdax.PublicClient();
-// const authClient = new gdax.AuthenticatedClient(apiKey, base64secret, passphrase, apiUri);
-const buyOrders = [];
-const sellOrders = [];
+const private = require('../private.json');
+const https = require('https');
+const crypto = require('crypto');
+const apiRest = 'api.gdax.com';
+const apiTime = '/time';
+const apiProducts = '/products';
+const apiAccounts = '/accounts';
 
 function getProducts() {
-  const description = 'get products';
-  const callback = (error, response, data) => {
-    console.log(description);
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log(data);
-  };
-  publicClient.getProducts(callback);
-}
-
-function getCurrencies() {
-  const description = 'get currencies';
-  const callback = (error, response, data) => {
-    console.log(description);
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log(data);
-  };
-  publicClient.getCurrencies(callback);
+    const options = {
+        host: apiRest,
+        port: 443,
+        path: apiProducts,
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'user-agent': 'bot'
+        }
+    };
+    const callback = (response) => {
+        response.on('error', (error) => {
+            console.log('error ' + error);
+        });
+        let body = '';
+        response.on('data', (data) => {
+            body += data;
+        });
+        response.on('end', () => {
+            // body = JSON.parse(body);
+            console.log('end ' + body);
+        });
+        console.log('code ' + response.statusCode);
+    };
+    const get = https.request(options, callback);
+    get.end();
 }
 
 function getAccounts() {
-  const description = 'get accounts';
-  const callback = (error, response, data) => {
-    console.log(description);
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log(data);
-  };
-  authClient.getAccounts(callback);
+    const body = JSON.stringify({
+        price: '1.0'
+    });
+    const restMethod = 'GET';
+    const requestPath = apiAccounts;
+    const apiKey = private.apiKey;
+    const secret = private.secret;
+    const timeStamp = Date.now() / 1000;
+    const passPhrase = private.passPhrase;
+    const prehash = timeStamp + restMethod + requestPath + body;
+    const secretKey = Buffer(secret, 'base64');
+    const hmac = crypto.createHmac('sha256', secretKey);
+    const signature = hmac.update(prehash).digest('base64');
+    const options = {
+        host: apiRest,
+        port: 443,
+        path: requestPath,
+        method: restMethod,
+        headers: {
+            'accept': 'application/json',
+            'user-agent': 'bot',
+            'CB-ACCESS-KEY': apiKey,
+            'CB-ACCESS-SIGN': signature,
+            'CB-ACCESS-TIMESTAMP': timeStamp,
+            'CB-ACCESS-PASSPHRASE': passPhrase
+        }
+    };
+    const callback = (response) => {
+        response.on('error', (error) => {
+            console.log('error ' + error);
+        });
+        let body = '';
+        response.on('data', (data) => {
+            body += data;
+        });
+        response.on('end', () => {
+            // body = JSON.parse(body);
+            console.log('end ' + body);
+        });
+        console.log('code ' + response.statusCode);
+    };
+    const get = https.request(options, callback);
+    get.end();
 }
 
-function getOrderbook(pair, api, feed) {
-  const book = new gdax.OrderbookSync([pair], api, feed);
-  console.log(book.books[pair].state());
+async function main() {
+    console.log('trading');
+    getAccounts();
+    await sleep(100);
+    console.log('done');
 }
 
-function listenExchange(pairs, uri) {
-  const socket = new gdax.WebsocketClient(pairs, uri);
-  const onMessage = (data) => {
-    if (data.type !== 'done' || data.reason !== 'filled') {
-      return;
-    }
-    console.log(data);
-  };
-  const onError = (error) => {
-    console.log('socket error');
-    console.log(error);
-  };
-  const onClose = () => {
-    console.log('socket closed');
-  };
-  socket.on('message', onMessage);
-  socket.on('error', onError);
-  socket.on('close', onClose);
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
 }
 
-function buy(price, size, product) {
-  const parameters = {
-    'price': price,
-    'size': size,
-    'product_id': product
-  };
-  const callback = () => {
-    console.log('todo');
-  };
-  const buyId = authClient.buy(parameters, callback);
-  buyOrders.push(buyId);
-}
-
-function sell(price, size, product) {
-  const parameters = {
-    'price': price,
-    'size': size,
-    'product_id': product
-  };
-  const callback = () => {
-    console.log('todo');
-  };
-  const sellId = authClient.sell(parameters, callback);
-  sellOrders.push(sellId);
-}
-
-function getOrders() {
-  const callback = () => {
-    console.log('todo');
-  };
-  authClient.getOrders(callback);
-}
-
-function checkOrder(orderId) {
-  const callback = () => {
-    console.log('todo');
-  };
-  authClient.getOrder(orderId, callback);
-}
-
-// getProducts();
-// getCurrencies();
-// getAccounts();
-// listenExchange([ethToUsd], api);
-// getOrderbook(ethToUsd, sandboxRest, sandboxFeed);
-// getOrderbook(ethToUsd, apiRest, apiFeed);
+main();
