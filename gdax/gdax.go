908f022c48996cb85323d35e983e3af03e29e37f
nmcmillan <nathannoob@gmail.com>
2018-02-21 18:50:56 -0800

    server gives client index page and pushes exchange ticker updates to it

--- gdax/gdax.go
+++ gdax/gdax.go
@@ -1,86 +1,91 @@
-
 package gdax
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"encoding/json"
 )
 
 const (
-	api = "https://api.gdax.com"
-	get = "GET"
+	api  = "https://api.gdax.com"
+	get  = "GET"
 	post = "POST"
 )
 
+// Candle product data
 type Candle struct {
-	Time float64
-	Low float64
-	High float64
-	Open float64
+	Time    float64
+	Low     float64
+	High    float64
+	Open    float64
 	Closing float64
-	Volume float64
+	Volume  float64
 }
 
 func request(method, url string) (*http.Client, *http.Request) {
 	client := &http.Client{}
-	request, e := http.NewRequest(method, url, nil)
-	ok(e)
+	request, err := http.NewRequest(method, url, nil)
+	ok(err)
 	request.Header.Add("Accept", "application/json")
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("User-Agent", "napa")
 	return client, request
 }
 
-func getRequest(url string) ([]byte) {
+func getRequest(url string) []byte {
 	client, request := request(get, url)
-	response, e := client.Do(request)
-	ok(e)
-	body, e := ioutil.ReadAll(response.Body)
-	ok(e)
+	response, err := client.Do(request)
+	ok(err)
+	body, err := ioutil.ReadAll(response.Body)
+	ok(err)
 	response.Body.Close()
 	return body
 }
 
-func GetCurrencies() (interface{}) {
+// GetCurrencies list of currencies
+func GetCurrencies() interface{} {
 	body := getRequest(api + "/currencies")
 	var decode interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	return decode
 }
 
-func GetBook(product string) (interface{}) {
+// GetBook map of level 2 product books
+func GetBook(product string) interface{} {
 	body := getRequest(api + "/products/" + product + "/book?level=2")
 	var decode interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	return decode
 }
 
-func GetTicker(product string) (interface{}) {
+// GetTicker map of product ticker
+func GetTicker(product string) interface{} {
 	body := getRequest(api + "/products/" + product + "/ticker")
 	var decode interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	return decode
 }
 
-func GetTrades(product string) (interface{}) {
+// GetTrades map of product trades
+func GetTrades(product string) interface{} {
 	body := getRequest(api + "/products/" + product + "/trades")
 	var decode interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	return decode
 }
 
-func GetHistory(product, start, end, granularity string) ([]Candle) {
+// GetHistory list of candles for product history
+func GetHistory(product, start, end, granularity string) []Candle {
 	body := getRequest(api + "/products/" + product + "/candles?start=" + start + "&end=" + end + "&granularity=" + granularity)
 	var decode []interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	candles := make([]Candle, 0)
-	for _ , list := range decode {
+	for _, list := range decode {
 		values := list.([]interface{})
 		candle := Candle{}
 		candle.Time = values[0].(float64)
@@ -94,16 +99,17 @@ func GetHistory(product, start, end, granularity string) ([]Candle) {
 	return candles
 }
 
-func GetStats(product string) (interface{}) {
+// GetStats map of 24 hour product statistics
+func GetStats(product string) interface{} {
 	body := getRequest(api + "/products/" + product + "/stats")
 	var decode interface{}
-	e := json.Unmarshal(body, &decode)
-	ok(e)
+	err := json.Unmarshal(body, &decode)
+	ok(err)
 	return decode
 }
 
-func ok(e error) {
-	if e != nil {
-		panic(e)
+func ok(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
--- napa.go
+++ napa.go
@@ -1,23 +1,55 @@
-
 package main
 
 import (
-	"fmt"
-	"time"
-	"./gdax"
-	"os"
-	"./analyst"
-	"./historian"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"./historian"
+	"github.com/gorilla/websocket"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type listenLock struct {
+	mu   *sync.Mutex
+	todo bool
+}
+
 const (
 	databaseDriver = "sqlite3"
-	databaseName = "./napa.db"
+	databaseName   = "./napa.db"
 )
 
-func remake() {
+var (
+	contents []byte
+)
+
+func app() {
+	// db, e := sql.Open(databaseDriver, databaseName)
+	// ok(e)
+
+	/* history := gdax.GetHistory("BTC-USD", "2018-02-17", "2018-02-18", "3600")
+	historian.ArchiveBtcUsd(db, history)
+	periods := historian.GetBtcUsd(db)
+	fmt.Println("MACD", analyst.MovingAverageConvergenceDivergence(6, 12, periods))
+	fmt.Println("RSI", analyst.RelativeStrengthIndex(7, periods)) */
+
+	/* gdax.GetCurrencies()
+		gdax.GetBook(product)
+		gdax.GetTicker(product)
+		gdax.GetTrades(product)
+	 	gdax.GetHistory(product, "2018-02-16", "2018-02-17", "3600")
+		gdax.GetStats(product) */
+
+	// db.Close()
+}
+
+func install() {
 	fmt.Println("deleting database if exists")
 	e := os.Remove(databaseName)
 	if e != nil && !os.IsNotExist(e) {
@@ -25,42 +57,139 @@ func remake() {
 	}
 	fmt.Println("creating database")
 	db, e := sql.Open(databaseDriver, databaseName)
- 	ok(e)
+	ok(e)
 	historian.CreateDb(db)
 	db.Close()
 }
 
-func app() {
-	db, e := sql.Open(databaseDriver, databaseName)
- 	ok(e)
-	
-	history := gdax.GetHistory("BTC-USD", "2018-02-17", "2018-02-18", "3600")
-	historian.ArchiveBtcUsd(db, history)
-	periods := historian.GetBtcUsd(db)
-	fmt.Println("MACD", analyst.MovingAverageConvergenceDivergence(6, 12, periods))
-	fmt.Println("RSI", analyst.RelativeStrengthIndex(7, periods))
-	
-	/* gdax.GetCurrencies()
-	gdax.GetBook(product)
-	gdax.GetTicker(product)
-	gdax.GetTrades(product)
- 	gdax.GetHistory(product, "2018-02-16", "2018-02-17", "3600")
-	gdax.GetStats(product) */
-	
-	db.Close()
+func indexPage(writer http.ResponseWriter, request *http.Request) {
+	writer.Write(contents)
+}
+
+func exchangeSocket(clientSocket *websocket.Conn, lock *sync.Mutex, listen *listenLock) {
+	fmt.Println("connecting to exchange")
+	url := "wss://ws-feed.gdax.com"
+	connection, _, err := websocket.DefaultDialer.Dial(url, nil)
+	ok(err)
+	js := json.RawMessage(`{"type":"subscribe", "product_ids":["BTC-USD"], "channels":["ticker"]}`)
+	err = connection.WriteJSON(js)
+	ok(err)
+	fmt.Println("listening to exchange")
+	for {
+		var proceed bool
+		listen.mu.Lock()
+		proceed = listen.todo
+		listen.mu.Unlock()
+		if !proceed {
+			break
+		}
+		var js interface{}
+		err := connection.ReadJSON(&js)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		message, ok := js.(map[string]interface{})
+		if !ok {
+			continue
+		}
+		messageType, ok := message["type"].(string)
+		if !ok {
+			continue
+		}
+		if messageType == "ticker" {
+			time, _ := message["time"].(string)
+			productID, _ := message["product_id"].(string)
+			price, _ := message["price"].(string)
+			side, _ := message["side"].(string)
+			clientMessage := fmt.Sprintf(`{"uid":"ticker", "time":"%s", "product_id":"%s", "price":"%s", "side":"%s"}`, time, productID, price, side)
+			go clientWrite(clientSocket, lock, clientMessage) // broadcast close exchange socket if fail ?
+		}
+	}
+	connection.Close()
+	fmt.Println("exchange connection closed")
+}
+
+func clientWrite(connection *websocket.Conn, lock *sync.Mutex, rawJs string) {
+	js := json.RawMessage([]byte(rawJs))
+	lock.Lock()
+	err := connection.WriteJSON(js)
+	lock.Unlock()
+	fmt.Println("sent", rawJs)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func clientRead(connection *websocket.Conn) {
+	var lock = &sync.Mutex{}
+	var exchangeLock = &listenLock{}
+	exchangeLock.mu = &sync.Mutex{}
+	exchangeLock.todo = false
+	for {
+		var js interface{}
+		err := connection.ReadJSON(&js)
+		if err != nil {
+			fmt.Println(err)
+			connection.Close()
+			break
+		}
+		message, ok := js.(map[string]interface{})
+		if !ok {
+			continue
+		}
+		uid, ok := message["uid"].(string)
+		if !ok {
+			continue
+		}
+		switch uid {
+		case "sub-exchange":
+			var current bool
+			exchangeLock.mu.Lock()
+			current = exchangeLock.todo
+			exchangeLock.todo = true
+			exchangeLock.mu.Unlock()
+			if current {
+				continue
+			}
+			go clientWrite(connection, lock, `{"uid":"log", "message":"subbing to exchange"}`)
+			go exchangeSocket(connection, lock, exchangeLock)
+		case "unsub-exchange":
+			go clientWrite(connection, lock, `{"uid":"log", "message":"unsubbing from exchange"}`)
+			exchangeLock.mu.Lock()
+			exchangeLock.todo = false
+			exchangeLock.mu.Unlock()
+		}
+	}
+}
+
+func clientSocket(writer http.ResponseWriter, request *http.Request) {
+	if request.Header.Get("Origin") != "http://"+request.Host {
+		http.Error(writer, "origin not allowed", 403)
+		return
+	}
+	upgrader := websocket.Upgrader{}
+	connection, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		http.Error(writer, "could not open websocket", 400)
+		return
+	}
+	go clientRead(connection)
 }
 
 func main() {
 	fmt.Println("napa bot")
-	arguments := os.Args
-	if len(arguments) > 1 {
-		com := arguments[1]
-		if com == "remake" {
-			remake()
-		}
-	} else {
-		app()
-	}
+
+	fmt.Println("loading files")
+	file, err := os.Open("napa.html")
+	ok(err)
+	contents, err = ioutil.ReadAll(file)
+	ok(err)
+
+	fmt.Println("listening and serving")
+	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/websocket", clientSocket)
+	http.ListenAndServe(":80", nil)
 }
 
 func sleep(seconds int32) {
